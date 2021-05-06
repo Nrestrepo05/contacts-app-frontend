417e02797b2594d1f817439de894e3645961ca67
nrestrepo05 <nrestrepo.r05@gmail.com>
2021-05-06 12:17:30 -0400

    add home page and move index to contacts/index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,144 +1,80 @@
-/* eslint-disable radix */
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import ContactInfo from '../components/ContactInfo';
-import ExitButton from '../components/ExitButton';
+import React from 'react';
+import Link from 'next/link';
 import Layout from '../components/Layout';
-import PrevNextButtons from '../components/PrevNextButtons';
-import PrimaryButton from '../components/PrimaryButton';
-import SearchBar from '../components/SearchBar';
-import SearchButton from '../components/SearchButton';
-import NotFound from '../components/NotFound';
 
-const Home = ({
-  contacts, pages, page, searchQuery,
-}) => {
-  const [buttonsHidden, setButtonsHidden] = useState(false);
-  const [nextButtonActive, setNextButtonActive] = useState(false);
-  const [prevButtonActive, setPrevButtonActive] = useState(false);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    if (page < pages) { setNextButtonActive(true); }
-    if (page >= pages) { setNextButtonActive(false); }
-    if (page > 1) { setPrevButtonActive(true); }
-    if (page <= 1) { setPrevButtonActive(false); }
-  });
-
-  const handleSearchButtonClick = () => {
-    setButtonsHidden(true);
-  };
-
-  const handleExitButtonClick = () => {
-    setButtonsHidden(false);
-    router.push('/');
-  };
-
-  const handleNextButtonClick = (e) => {
-    e.preventDefault();
-    let url;
-    if (searchQuery) {
-      url = `/?page=${page + 1}&search=${searchQuery}`;
-    } else {
-      url = `/?page=${page + 1}`;
-    }
-
-    router.push(url);
-  };
-
-  const handlePrevButtonClick = (e) => {
-    e.preventDefault();
-    let url;
-    if (searchQuery) {
-      url = `/?page=${page - 1}&search=${searchQuery}`;
-    } else {
-      url = `/?page=${page - 1}`;
-    }
-
-    router.push(url);
-  };
-
+const Home = () => {
   return (
     <>
       <Layout>
-        <div>
-          <SearchBar hidden={!buttonsHidden} />
-          <ExitButton onClick={handleExitButtonClick} small="true" hidden={!buttonsHidden} />
-          <PrimaryButton hidden={buttonsHidden}>+ New Contact</PrimaryButton>
-          <SearchButton onClick={handleSearchButtonClick} hidden={buttonsHidden} />
-        </div>
-        {contacts.length < 1
-          ? (<NotFound title="There's Nothing yet" subtitle="Create the first!" />)
-          : ''}
-        {contacts.map((contact) => (
-          <ContactInfo
-            name={contact.name}
-            lastName={contact.last_name}
-            email={contact.email}
-            id={contact._id}
-            key={contact._id}
-          />
-        ))}
-        <PrevNextButtons
-          prev={prevButtonActive}
-          next={nextButtonActive}
-          prevOnClick={handlePrevButtonClick}
-          nextOnClick={handleNextButtonClick}
-        />
+        <section>
+
+          <h1>Welcome to Contacts App!</h1>
+          <p>
+            This is an app that allows you to create and edit contacts.
+            <br />
+            It also allows you to search for the contacts stored by any of its fields.
+          </p>
+          <p>
+            This app is built in Next.JS, and it consumes an api written in node.JS
+          </p>
+          <p>
+            If you want to see the code of this app, you can enter&nbsp;
+            <a href="https://github.com/Nrestrepo05/contacts-app-frontend">this link</a>
+          </p>
+          <p>
+            If you want to try the api you can enter&nbsp;
+            <a href="https://contacts-api-nrestrepo05.herokuapp.com/contacts">this link</a>
+          </p>
+          <p>
+            and if you want to see the api code you can enter&nbsp;
+            <a href="https://github.com/Nrestrepo05/contacts-app-api">this link</a>
+          </p>
+
+          <p><strong>Thanks for entering. I hope you like it!</strong></p>
+
+          <Link href="/contacts">
+            <a className="button">Go to Contacts App!</a>
+          </Link>
+        </section>
       </Layout>
       <style jsx>
         {`
-          div {
-            max-width: 100%;
-            min-width: 100%;
-            display: flex;
-            margin-bottom: 30px;
-            align-items: center;
-            justify-content: center;
+          section {
+            text-align: center;
+            margin-top: 30px;
           }
-          @media screen and (min-width: 1024px) {
-            div {
-              max-width: 65%;
-              min-width: 65%;
-            }
+          p {
+            margin-top: 10px;
+            color: #375D81;
+          }
+          h1 {
+            color: #183152;
+          }
+          p strong {
+            color: #183152;
+          }
+          a {
+            color: #183152;
+          }
+          a.button {
+            color: white;
+            background: #375D81;
+            min-width: 180px;
+            max-width: 180px;
+            padding: 10px 20px;
+            margin-top: 20px;
+            display: inline-block;
+            border-radius: 7px;
+            text-decoration: none;
+            font-weight: bold;
+          }
+          a.button:focus {
+            opacity: 0.5;
           }
         `}
       </style>
     </>
   );
 };
-
-export async function getServerSideProps({ query }) {
-  let page = 1;
-  let searchQuery = null;
-  const baseUrl = process.env.BACKEND_HOST;
-  let url;
-
-  if (query.page) { page = query.page; }
-
-  if (query.search) {
-    searchQuery = query.search;
-    url = `${baseUrl}/contacts?page=${page}&search=${searchQuery}`;
-  } else {
-    url = `${baseUrl}/contacts?page=${page}`;
-  }
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  const { contacts } = data.body;
-  let { pages } = data.body;
-
-  pages = parseInt(pages);
-  page = parseInt(page);
-
-  return {
-    props: {
-      contacts, pages, page, searchQuery,
-    },
-  };
-}
 
 export default Home;
