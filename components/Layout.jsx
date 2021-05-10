c41671b90548dd00262649527d2dd35e6637aa82
Nicolas Restrepo <nrestrepo.r05@gmail.com>
2021-05-10 12:37:26 -0400

    add favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout = ({ children, center }) => {
+const Layout = ({ children, center, title }) => {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📘</text></svg>" />
+      </Head>
       <Header />
       <main className={center ? 'center' : ''}>
         {children}
